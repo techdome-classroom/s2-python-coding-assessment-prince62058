4fd4822bfbb50ff03214616559a0f420a0ca73f3
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2024-10-08 14:59:50 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,4 +1,5 @@
-def roman_to_int(s):
+class Solution:
+    def roman_to_int(s):
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
