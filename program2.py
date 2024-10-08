70edaff97999bc02cc2f0237a6fdfb113d08efcc
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2024-10-08 14:36:28 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,16 +1,25 @@
-# program2.py
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
         total = 0
         prev_value = 0
 
-        for char in reversed(s):
+        for char in s:
             current_value = roman_map[char]
-            if current_value < prev_value:
-                total -= current_value
+            if current_value > prev_value:
+                total += current_value - 2 * prev_value
             else:
                 total += current_value
+            
             prev_value = current_value
 
         return total
