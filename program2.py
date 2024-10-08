b6642f4f050f57133f80e74cafd7a80c1e774b26
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2024-10-08 14:58:57 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,18 +1,14 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman_to_int = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
-        }
-        total = 0
-        prev_value = 0
+def roman_to_int(s):
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
 
-        for char in s:
-            curr_value = roman_to_int[char]
-            if curr_value > prev_value:
-                total += curr_value - 2 * prev_value
-            else:
-                total += curr_value
-            prev_value = curr_value
-        
-        return total
+    for char in reversed(s):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
